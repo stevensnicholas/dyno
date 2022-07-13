c636174ca270e9c3853cbebdaa2ab7c72267e602
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-13 22:04:10 +1000

    Fix secreity issue - make immutable

--- terraform/aws_only/ecr.tf
+++ terraform/aws_only/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "image_repository" {
   name                 = "dyno_image_repository"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
