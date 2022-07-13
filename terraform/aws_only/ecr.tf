99e9a0c4062098d0a3241a12ad342ea9acf715bb
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-13 21:43:06 +1000

    Add ecr so we can push docker images for lambda

--- terraform/aws_only/ecr.tf
+++ terraform/aws_only/ecr.tf
@@ -1,7 +1,10 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Add Kms ley
+=======
+>>>>>>> Add ecr so we can push docker images for lambda
 resource "aws_kms_key" "ecr_kms" {
   enable_key_rotation = true
 }
@@ -12,6 +15,7 @@ resource "aws_ecr_repository" "image_repository" {
   name                 = "dyno_image_repository"
   image_tag_mutability = "IMMUTABLE"
 =======
+<<<<<<< HEAD
 =======
 >>>>>>> Add Kms ley
 resource "aws_ecr_repository" "image_repository" {
@@ -22,6 +26,12 @@ resource "aws_ecr_repository" "image_repository" {
 =======
   image_tag_mutability = "IMMUTABLE"
 >>>>>>> Fix secreity issue - make immutable
+=======
+resource "aws_ecr_repository" "image_repository" {
+  name                 = "dyno_image_repository"
+  image_tag_mutability = "MUTABLE"
+>>>>>>> Add ecr so we can push docker images for lambda
+>>>>>>> Add ecr so we can push docker images for lambda
 
   image_scanning_configuration {
     scan_on_push = true
@@ -29,8 +39,11 @@ resource "aws_ecr_repository" "image_repository" {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> Add Kms ley
+=======
+>>>>>>> Add ecr so we can push docker images for lambda
 
   encryption_configuration {
     encryption_type = "KMS"
@@ -45,4 +58,10 @@ resource "aws_ecr_repository" "image_repository" {
 =======
 >>>>>>> Add Kms ley
 }
+<<<<<<< HEAD
 >>>>>>> Fix secreity issue - make immutable
+=======
+=======
+}
+>>>>>>> Add ecr so we can push docker images for lambda
+>>>>>>> Add ecr so we can push docker images for lambda
