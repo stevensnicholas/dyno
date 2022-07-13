3dd9009f4d35b87ff89d596a412d98a6d34f4f92
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-13 22:07:50 +1000

    Add Kms ley

--- terraform/aws_only/ecr.tf
+++ terraform/aws_only/ecr.tf
@@ -1,36 +1,8 @@
-<<<<<<< HEAD
-resource "aws_kms_key" "ecr_kms" {
-  enable_key_rotation = true
-}
-
-
 resource "aws_ecr_repository" "image_repository" {
   name                 = "dyno_image_repository"
-  image_tag_mutability = "IMMUTABLE"
-=======
-resource "aws_ecr_repository" "image_repository" {
-  name                 = "dyno_image_repository"
-<<<<<<< HEAD
   image_tag_mutability = "MUTABLE"
->>>>>>> Add ecr so we can push docker images for lambda
-=======
-  image_tag_mutability = "IMMUTABLE"
->>>>>>> Fix secreity issue - make immutable
 
   image_scanning_configuration {
     scan_on_push = true
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-  encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = aws_kms_key.ecr_kms.key_id
-  }
 }
-=======
-}
->>>>>>> Add ecr so we can push docker images for lambda
-=======
-}
->>>>>>> Fix secreity issue - make immutable
