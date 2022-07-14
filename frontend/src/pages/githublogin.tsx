25431737304b0408ada4fee2af04690eb46dc8e0
dn1470 <youngjuchae14@gmail.com>
2022-07-14 23:18:45 +1000

    prettier fix

--- frontend/src/pages/githublogin.tsx
+++ frontend/src/pages/githublogin.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 
 const GitHublogin = () => {
-  return(
+  return (
     <div>
       <p>Login with Github</p>
     </div>
   );
-}
+};
 
-export default GitHublogin
+export default GitHublogin;
