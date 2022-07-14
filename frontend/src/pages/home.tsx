88bc65e0f30b67373ac25e2f778a04f015b1164d
dn1470 <youngjuchae14@gmail.com>
2022-07-14 22:25:06 +1000

    typescript skeleton pages

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+
+const Home = () => {
+  return(
+    <div>
+      <p>Home</p>
+    </div>
+  );
+}
+
+export default Home
