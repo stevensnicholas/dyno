c1ec4eec2b91effd7cf825ebacae73d40b553ee4
dn1470 <youngjuchae14@gmail.com>
2022-07-14 23:21:41 +1000

    prettier fix

--- frontend/src/navigation.tsx
+++ frontend/src/navigation.tsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/home';
 import GitHublogin from './pages/githublogin';
- 
+
 export function Navigation() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<GitHublogin />} />
-    </Routes> 
+    </Routes>
   );
-
-};
+}
