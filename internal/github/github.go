bc10c55673f94c45525b7d160b0bd7dc78914f8f
thefishua <josh.fishua@gmail.com>
2022-07-15 08:49:12 +1000

    Added github library with a stub github test file

--- internal/github/github.go
+++ internal/github/github.go
@@ -1,1 +1,57 @@
 package github
+
+import (
+	"context"
+	"golang.org/x/oauth2"
+	"github.com/google/go-github/v45/github"
+)
+
+// Creates an Authenticated Client 
+// Inputs: token is the user token 
+// Returns the Client 
+func CreateClient(ctx context.Context, token string) *github.Client {
+	authToken := token
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: authToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	
+	return client
+}
+// Gets the specific repo provided by the owner and given repo name.
+// Inputs: owner is the owner of the repo, repoName is the name of the repo.
+// Returns: the specified repo as a *github.Repository 
+func GetRepo(ctx context.Context, client *github.Client, owner string, repoName string) *github.Repository {
+	repo, _, err := client.Repositories.Get(ctx, owner, repoName)
+	if err != nil {
+		panic(err)
+	}
+	return repo
+}
+// Creates an IssueRequest that provides the body of the vulnerabilities within the software.
+// Parses the files from fuzzing into an IssueRequest.
+// Returns an IssueRequest.
+func CreateIssueRequest(title *string, body *string, labels *[]string, assignee *string, state *string, milestone *int) *github.IssueRequest {
+	newIssueRequest := &github.IssueRequest{}
+	
+	if title != nil {
+		newIssueRequest.Title = title
+	}
+	if body != nil {
+		newIssueRequest.Body = body
+	}
+	if labels != nil {
+		newIssueRequest.Labels = labels
+	}
+	if assignee != nil {
+		newIssueRequest.Assignee = assignee
+	}
+	if state != nil {
+		newIssueRequest.State = state
+	}
+	if milestone != nil {
+		newIssueRequest.Milestone = milestone
+	}
+	return newIssueRequest
+}
