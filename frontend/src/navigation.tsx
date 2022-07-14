e429d311f4c46b6c2a0268eae93078e4df0245aa
dn1470 <youngjuchae14@gmail.com>
2022-07-14 23:13:36 +1000

    prettier fix

--- frontend/src/navigation.tsx
+++ frontend/src/navigation.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Home  from "./pages/home"
-import GitHublogin from "./pages/githublogin"
+import Home from './pages/home';
+import GitHublogin from './pages/githublogin';
 
-export function Navigation(){
-  return(
-   <Routes>
-      <Route path='/home' element={<Home />} />
-      <Route path='/login'  element={<GitHublogin />} />
-   </Routes> 
+export function Navigation(){ 
+  return( 
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login"  element={<GitHublogin />} />
+    </Routes> 
   );
 }
