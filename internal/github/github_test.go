2c3ac4cba8c063073bb6a76b642156bb1bf6d3e3
thefishua <josh.fishua@gmail.com>
2022-07-15 15:21:27 +1000

    Added testing to github.go functions

--- internal/github/github_test.go
+++ internal/github/github_test.go
@@ -1,2 +1,39 @@
 package github_test
 
+import (
+	"testing"
+	"github.com/joho/godotenv"
+	"os"
+)
+func getEnv() string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	return os.Getenv("GITHUB_API")
+}
+
+func TestValidClient(t *testing.T) {
+	
+}
+
+func TestInvalidClient() {
+
+}
+
+func TestValidGetRepo() {
+
+}
+
+func TestInvalidGetRepo() {
+
+}
+
+func TestValidCreateIssueRequest() {
+
+}
+
+func TestInValidCreateIssueRequest() {
+	
+}
