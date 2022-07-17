794c8b6d528ed65d73cbbb32ea381f8865a06f29
thefishua <josh.fishua@gmail.com>
2022-07-17 16:33:11 +1000

    Created DynoIssue struct for decoupling

--- internal/issues/issues.go
+++ internal/issues/issues.go
@@ -1,1 +1,26 @@
 package issues
+
+type DynoIssue struct {
+	Title *string `json:"title,omitempty"`
+	Body *DynoIssueBody `json:"body,omitempty"`
+	Assignee *string `json:"assignee,omitempty"`
+	Labels *string `json:"labels,omitempty"`
+}
+
+type DynoIssueBody struct {
+	Title *string `json:"title,omitempty"`
+	Details *string  `json:"details,omitempty"`
+	Visualizer *string  `json:"visualizer,omitempty"`
+	Method *string  `json:"method,omitempty"`
+	MethodInformation *DynoMethodInformation `json:"methodInformation,omitempty"`
+	TimeDelay *string  `json:"timeDelay,omitempty"`
+	AsyncTime *string `json:"asyncTime,omitempty"`
+	PreviousResponse *string  `json:"previousResponse,omitempty"`
+}
+
+type DynoMethodInformation struct {
+	AcceptedResponse *string `json:"acceptedResponse,omitempty"`
+	Host *string `json:"host,omitempty"`
+	ContentType *string `json:"contentType,omitempty"`
+	Request *string `json:"request,omitempty"`
+}
