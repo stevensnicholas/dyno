cf1880d27e5402761681302a758c1ff53b08cdcb
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-19 10:35:54 +1000

    add alias

--- terraform/aws_only/ecr.tf
+++ terraform/aws_only/ecr.tf
@@ -2,6 +2,10 @@ resource "aws_kms_key" "ecr_kms" {
   enable_key_rotation = true
 }
 
+resource "aws_kms_alias" "ecr_kms_alias" {
+  name          = "alias/ecr_kms_alias"
+  target_key_id = aws_kms_key.ecr_kms.key_id
+}
 
 resource "aws_ecr_repository" "image_repository" {
   name                 = "dyno_image_repository"
@@ -13,6 +17,6 @@ resource "aws_ecr_repository" "image_repository" {
 
   encryption_configuration {
     encryption_type = "KMS"
-    kms_key         = aws_kms_key.ecr_kms.key_id
+    kms_key         = aws_kms_alias.ecr_kms_alias.target_key_arn
   }
 }
