62c07acbd0754f98798f78f2e22908082716ecca
Rubaiat <iamcrubaiat@gmail.com>
2022-07-21 18:02:04 +1000

    formatting the code

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Photo from "../images/logo/login-logo.png"
-import "../css/style.css"
+import Photo from '../images/logo/login-logo.png';
+import '../css/style.css';
 
 const Login = () => {
   return (
@@ -10,18 +10,27 @@ const Login = () => {
           <div className="col s12 z-depth-4 card-panel">
             <div className="row">
               <div className="input-field col s12 center">
-                <img src={Photo} alt="" className="circle responsive-img valign profile-image-login" />
+                <img
+                  src={Photo}
+                  alt=""
+                  className="circle responsive-img valign profile-image-login"
+                />
                 <p className="center login-form-text">Sign In</p>
                 <h4 className="login-form-text">Use your Github Account</h4>
               </div>
             </div>
             <div className="row">
               <div className="login-box">
-                <a href="#" className="btn-large waves-effect waves-teal col s12">
+                <a
+                  href="#"
+                  className="btn-large waves-effect waves-teal col s12"
+                >
                   <span />
                   <span />
                   <span />
-                  <span />Login</a>
+                  <span />
+                  Login
+                </a>
               </div>
             </div>
           </div>
