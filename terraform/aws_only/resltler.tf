cc92e14d7b096e6f35dbe1a336a7df7a76386dc3
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-22 11:59:37 +1000

    Add in lambda

--- terraform/aws_only/resltler.tf
+++ terraform/aws_only/resltler.tf
@@ -3,16 +3,36 @@ resource "aws_cloudwatch_log_group" "lambda_restler" {
   retention_in_days = 30
 }
 
+resource "aws_iam_role" "lambda_restler" {
+  name = "${var.deployment_id}-lambda-execution-restler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_restler" {
+  role       = aws_iam_role.lambda_restler.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "lambda_restler" {
   function_name = "${var.deployment_id}-restler-fuzzer"
   image_uri     = "${aws_ecr_repository.image_repository.repository_url}:latest"
-
-  runtime = "python3.9"
-  handler = "app.handler"
+  handler       = "app.handler"
+  timeout       = 60
 
   tracing_config {
     mode = "Active"
   }
 
-  role = aws_iam_role.lambda_exec_test.arn
+  role = aws_iam_role.lambda_restler.arn
 }
