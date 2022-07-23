0886fd08681ad5ec136ee642d740598f29ec4b39
Rubaiat <iamcrubaiat@gmail.com>
2022-07-23 19:22:23 +1000

    Fix style issue

--- frontend/src/pages/errors/500.tsx
+++ frontend/src/pages/errors/500.tsx
@@ -10,14 +10,18 @@ const Error500 = () => {
         <div id={styles.minimize_circle} className={styles.circle}></div>
         <div id={styles.maximize_circle} className={styles.circle}></div>
       </div>
-      <div className='row'>
+      <div className="row">
         <div id={styles.site_layout_example_top} className="col s12">
-          <p className={styles.caption_uppercase}><h5 className={'center'}>Internal server error</h5></p>
+          <p className={styles.caption_uppercase}>
+            <h5 className={'center'}>Internal server error</h5>
+          </p>
         </div>
         <div id={styles.site_layout_example_right} className="col s12 m12 l12">
           <div className="row center">
-            <h1 className={(styles.text_long_shadow) + "col s12 mt-3"}>500</h1>
-            <p className="center white-text">Something has gone seriously wrong. Please try later.</p>
+            <h1 className={styles.text_long_shadow + 'col s12 mt-3'}>500</h1>
+            <p className="center white-text">
+              Something has gone seriously wrong. Please try later.
+            </p>
           </div>
         </div>
       </div>
