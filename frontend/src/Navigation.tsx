ea17d2f4b423d73f171f34feb1ad0cb33f6b260d
Rubaiat <iamcrubaiat@gmail.com>
2022-07-23 19:16:11 +1000

    Error 500 page

--- frontend/src/Navigation.tsx
+++ frontend/src/Navigation.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/login/Login';
 import Dashboard from './pages/Dashboard';
+import Error500 from './pages/errors/500';
 
 const Navigation = () => {
   return (
@@ -10,6 +11,7 @@ const Navigation = () => {
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/500" element={<Error500 />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/errors/500.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import styles from '../errors/500.module.css';
+
+const Error500 = () => {
+  return (
+    <div className={styles.browser_window}>
+      <div className={styles.top_bar}>
+        <div id={styles.close_circle} className={styles.circle}></div>
+        <div id={styles.minimize_circle} className={styles.circle}></div>
+        <div id={styles.maximize_circle} className={styles.circle}></div>
+      </div>
+      <div className='row'>
+        <div id={styles.site_layout_example_top} className="col s12">
+          <p className={styles.caption_uppercase}><h5 className={'center'}>Internal server error</h5></p>
+        </div>
+        <div id={styles.site_layout_example_right} className="col s12 m12 l12">
+          <div className="row center">
+            <h1 className={(styles.text_long_shadow) + "col s12 mt-3"}>500</h1>
+            <p className="center white-text">Something has gone seriously wrong. Please try later.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Error500;
