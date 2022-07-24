f04d603dcae36635f109f6de82181a8b66c125b6
thefishua <josh.fishua@gmail.com>
2022-07-24 20:15:57 +1000

    Updated issues

--- internal/platform/github_platform.go
+++ internal/platform/github_platform.go
@@ -1,8 +1,8 @@
 package platform
 
 import (
-	"dyno/internal/issue"
 	"context"
+	"dyno/internal/issue"
 	"github.com/google/go-github/v45/github"
 	"golang.org/x/oauth2"
 )
@@ -10,7 +10,7 @@ import (
 // Creates an Authenticated Client for communitcation with github
 // Inputs: token is the user token
 // Returns the Client
-func CreateClient(ctx context.Context, token *string) *github.Client {
+func CreateGithubClient(ctx context.Context, token *string) *github.Client {
 	authToken := token
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: *authToken},
@@ -33,17 +33,17 @@ func GetRepo(ctx context.Context, client *github.Client, owner *string, repoName
 }
 
 // FormatFuzzBody formats the structs of DynoResults and creates a string to be used within
-// a github issue and be presented to the user 
+// a github issue and be presented to the user
 // Inputted is a DynoResult and outputted is a string used for the github issue
 func FormatFuzzBody(dynoIssue *issue.DynoIssue) *string {
 	body := "# " + *dynoIssue.Body.Title + "\n\n" + *dynoIssue.Details + "\n\n" + *dynoIssue.Visualizer + "\n"
-	if dynoIssue.Body.MethodInformation.ContentType != nil && *dynoIssue.Body.MethodInformation.ContentType != ""{
+	if dynoIssue.Body.MethodInformation.ContentType != nil && *dynoIssue.Body.MethodInformation.ContentType != "" {
 		body = body + "\n" + *dynoIssue.Body.Method + "\n" + "\n" + "- " + *dynoIssue.Body.MethodInformation.AcceptedResponse + "\n" + "- " + *dynoIssue.Body.MethodInformation.Host + "\n" + "- " + *dynoIssue.Body.MethodInformation.ContentType
 	} else {
 		body = body + "\n" + *dynoIssue.Body.Method + "\n" + "\n" + "- " + *dynoIssue.Body.MethodInformation.AcceptedResponse + "\n" + "- " + *dynoIssue.Body.MethodInformation.Host
 	}
 	if dynoIssue.Body.MethodInformation.Request != nil && *dynoIssue.Body.MethodInformation.Request != "" {
-		body = body + "\n" + "- " + *dynoIssue.Body.MethodInformation.Request 
+		body = body + "\n" + "- " + *dynoIssue.Body.MethodInformation.Request
 	}
 	body = body + "\n" + "\n" + *dynoIssue.Body.TimeDelay + "\n" + *dynoIssue.Body.AsyncTime + "\n" + "\n" + *dynoIssue.Body.PreviousResponse
 	body = body + "\n"
