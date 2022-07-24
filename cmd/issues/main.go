1322115b89853cd52d636d18237c39798e1c6839
thefishua <josh.fishua@gmail.com>
2022-07-24 21:04:00 +1000

    Added terraform and begun to create lambda function:

--- cmd/issues/main.go
+++ cmd/issues/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
 func main() {
+<<<<<<< HEAD
 	println("This is where the lambda for the issues part of the project will be")
+=======
+	lambda.Start(Handler)
+}
+
+func Handler() {
+	fmt.Println("Function invoked!")
+>>>>>>> 8e0ffd5 (Added terraform and begun to create lambda function:)
 }
