313202d8a8a81a85620257cb18d55d1a26135329
Ubuntu <ubuntu@ip-172-31-22-252.ap-southeast-2.compute.internal>
2022-07-25 01:22:47 +0000

    added initial resource for s3 and sqs to get files

---
+++ terraform/aws_only/openApiFiles_queue.tf
@@ -0,0 +1,50 @@
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "client-openAPI-files"
+}
+
+resource "aws_sqs_queue" "sqsQueue" {
+  name                      = "openApiFilesQueue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_sqs_queue_policy" "policy" {
+  queue_url = aws_sqs_queue.sqsQueue.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "example-ID",
+  "Statement": [
+    {
+      "Sid": "example-statement-ID",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Action": "SQS:SendMessage",
+      "Resource": "${aws_sqs_queue.sqsQueue.arn}",
+      "Condition": {
+        "StringEquals": {
+          "aws:SourceAccount": "${data.aws_caller_identity.current.account_id}"
+        },
+        "ArnLike": {
+          "aws:SourceArn": "${aws_s3_bucket.bucket.id}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
