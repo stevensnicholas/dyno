8953fbf25f913468a753a01e2e91ad4bc44aa226
stevensnicholas <n.j.stevens@student.unsw.edu.au>
2022-07-26 22:20:59 +1000

    Move Restler IAM into main (#120)

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -10,3 +10,8 @@ output "static_react_bucket" {
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
+
+output "lambda_restler_iam_arn" {
+  value = aws_iam_role.lambda_restler.arn
+
+}
