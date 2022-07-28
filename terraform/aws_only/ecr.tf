059570db3b1d9c9c2b6f9cc64a1f737f0860d0d2
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-28 19:43:45 +1000

    Rip out all not working parts

--- terraform/aws_only/ecr.tf
+++ terraform/aws_only/ecr.tf
@@ -1,26 +1,3 @@
-resource "aws_kms_key" "ecr_kms" {
-  enable_key_rotation = true
-}
-
-resource "aws_kms_alias" "ecr_kms_alias" {
-  name          = "alias/ecr_kms_alias"
-  target_key_id = aws_kms_key.ecr_kms.key_id
-}
-
-resource "aws_ecr_repository" "image_repository" {
-  name                 = "dyno_image_repository"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-    kms_key         = aws_kms_alias.ecr_kms_alias.target_key_arn
-  }
-}
-
 resource "aws_kms_key" "ecr_kms_named" {
   enable_key_rotation = true
 }
@@ -40,6 +17,6 @@ resource "aws_ecr_repository" "image_repository_named" {
 
   encryption_configuration {
     encryption_type = "KMS"
-    kms_key         = aws_kms_alias.ecr_kms_alias.target_key_arn
+    kms_key         = aws_kms_alias.ecr_kms_alias_named.target_key_arn
   }
 }
