1cbd74851abee5c63f2d7585733a69556169bfbb
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-07-29 15:29:45 +1000

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,6 +5,7 @@ variable "deployment_id" {
 
 variable "restler_image_tag" {
   type        = string
+  default = "117712065617.dkr.ecr.ap-southeast-2.amazonaws.com/main_dyno_image_repository:9270505710dab7540a8340efc24fef311b75552e"
   description = <<EOT
   restler image tag to pull, if this is the first run then you need to supply an image from another ecr
   For example to get the current latest pushed image on main (production) run the following comand
