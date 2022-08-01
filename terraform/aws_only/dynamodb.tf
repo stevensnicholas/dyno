a7a6a8dcb9e1178cf6a3950d35250f86c0c7c8e2
Rubaiat <iamcrubaiat@gmail.com>
2022-08-01 19:09:23 +1000

    fix dynamodb.tf

--- terraform/aws_only/dynamodb.tf
+++ terraform/aws_only/dynamodb.tf
@@ -1,46 +1,8 @@
-variable "dyno_table"{
-  type = string
-  description = "Table for dyno app"
-}
-
-resource "aws_dynamodb" "dyno_table"{
-  name = var.dyno_table
+resource "aws_dynamodb_table" "dyno_table" {
+  name = "${var.deployment_id}-fuzz-results"
+  hash_key       = "clientID"
   attribute {
     name = "clientID"
     type = "S"
   }
-  attribute {
-    name = "Title"
-    type = "S"
-  }
-  attribute {
-    name = "Details"
-    type = "S"
-  }
-  attribute {
-    name = "Visualizer"
-    type = "S"
-  }
-  attribute {
-    name = "Body"
-    type = ""
-  }
-  attribute {
-    name = "Assignee"
-    type = "S"
-  }
-  atrribute {
-    name = "Labels"
-    type = "SS"
-  }
-
-  atrribute {
-    name = "State"
-    type = "S"
-  }
-  atrribute {
-    name = "Milestone"
-    type = "N"
-  }
 }
-
