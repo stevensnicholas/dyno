2ea44113a9d15a63d1b500def7b039308306e12a
Varun Singh <varun.singh@aroflo.com>
2022-08-01 19:40:52 +1000

    added code to send message to sqs

--- internal/endpoints/fuzz.go
+++ internal/endpoints/fuzz.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/google/uuid"
 	"github.com/swaggest/rest/web"
 	"github.com/swaggest/usecase"
@@ -37,16 +38,51 @@ func Fuzz(service *web.Service) {
 
 		u := uuid.New()
 		key := fmt.Sprintf("Open-Api-Files/%s", u.String())
+		bucket_name:="test-store-swagger"
 		_, ierr := uploader.Upload(&s3manager.UploadInput{
 			//will use the bucket name as variable
-			Bucket: aws.String("test-store-swagger"),
+			Bucket: aws.String(bucket_name),
 			Key:    aws.String(key),
 			Body:   bod,
 		})
 
+		s3URI:=fmt.Sprintf("{\"s3_location\":\"s3://%s/%s\"}",bucket_name,key)
+		
 		if ierr != nil {
 			logger.Infof("There was an issue uploading to s3: %s", ierr.Error())
 		}
+
+		//add sqs message
+		sess2 := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+
+		svc := sqs.New(sess2)
+
+		// URL to our queue- need terraform variable here
+		qURL := "https://sqs.ap-southeast-2.amazonaws.com/117712065617/varun-test-sqs-queue"
+
+		result, err := svc.SendMessage(&sqs.SendMessageInput{
+				DelaySeconds: aws.Int64(10),
+				MessageAttributes: map[string]*sqs.MessageAttributeValue{
+						"Client": &sqs.MessageAttributeValue{
+								DataType:    aws.String("String"),
+								StringValue: aws.String("Will be the client name from env variable"),
+						},
+						"Content": &sqs.MessageAttributeValue{
+								DataType:    aws.String("String"),
+								StringValue: aws.String("Client OpenApi File"),
+						},
+				},
+				MessageBody: aws.String(s3URI),
+				QueueUrl:    &qURL,
+		})
+
+		if err != nil {
+				logger.Infof("There was an issue adding event to sqs: %s", err.Error())
+		}
+
+		logger.Infof("Success", *result.MessageId)
 
 		return nil
 	}
