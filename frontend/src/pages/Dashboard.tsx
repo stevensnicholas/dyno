e245409c6b26621411fadb30642e4841d410571e
dn1470 <youngjuchae14@gmail.com>
2022-08-01 22:27:20 +1000

    github login redirection

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AppClient } from '../client';
+import { Loading } from '../components/Loading/Loading';
 
-const Dashboard = () => {
+interface Props {
+  client: AppClient;
+}
+
+const Dashboard = (props: Props) => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const code = window.location.search.substring(1).split('=')[2];
+    const url = `http://localhost:8080/login?code=${code}`;
+    props.client.default.endpointsAuthentication(url).then((res) => {
+      setLoggedIn(true);
+    });
+  }, [props]);
   return (
-    <div>
-      <p>Dashboard</p>
-    </div>
+    <>
+      {loggedIn ? (
+        <div>
+          <p>Dashboard</p>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 };
 
