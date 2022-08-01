aed9b1a7676b4a792de1d3b32b03246382457200
thefishua <josh.fishua@gmail.com>
2022-08-01 20:41:04 +1000

    Issues tf with terraform to create SNS -> SQS -> Lambda

--- cmd/issues/main.go
+++ cmd/issues/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
 func main() {
-	println("Issue lambda function")
+	lambda.Start(handler)
+}
+
+// Testing SQS message is sent to Lambda with the correct message contents 
+func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	fmt.Println("Function invoked!")
+	if len(sqsEvent.Records) == 0 {
+		fmt.Println("No SQS events")
+	}
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+	}
+	return nil
 }
