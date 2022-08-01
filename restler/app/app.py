b73c5afffee25cba522a8a084e44f9c379246081
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-08-01 22:57:09 +1000

    alter engine_setting

--- restler/app/app.py
+++ restler/app/app.py
@@ -25,20 +25,24 @@ def handler(event, context):
         json.dump(event['swagger_file'],f)
     print("swagger file saved")
     run(restler_compile_cmd, shell=True)
+    with open('/tmp/Compile/engine_settings.json','r') as f:
+        engine_settings = json.load(f)
+        engine_settings["garbage_collection_interval"] = 0
+    with open('/tmp/Compile/engine_settings.json','w') as f:
+        json.dump('/tmp/Compile/engine_settings.json')
     print("swagger file complied")
     run(restler_fuzz_cmd, shell=True)
     print("fuzzing-lean complete")
     s3 = boto3.client("s3")
     bucket_name = os.environ['results_upload_s3_bucket']
     random_prefix = uuid.uuid4()
-    results_key = f"{random_prefix}/results.zip"
-    logs_key = f"{random_prefix}/logs.zip"
-    shutil.make_archive("/tmp/results", 'zip', "/tmp/FuzzLean")
-    shutil.make_archive("/tmp/logs", 'zip', "/tmp/RestlerLogs")
-    response_results = s3.upload_file("/tmp/results.zip", bucket_name, logs_key)
-    response_logs = s3.upload_file("/tmp/logs.zip", bucket_name, results_key)
-    print(f"S3 upload response for results: {response_results}")
-    print(f"S3 upload response for logs: {response_logs}")
+    for folder,prefix in [("RestlerLogs","logs"),("FuzzLean","results"),("Compile","compile")]:
+        key = f"{random_prefix}/{prefix}.zip"
+        shutil.make_archive(f"/tmp/{prefix}", 'zip', f"/tmp/{folder}")
+        response = s3.upload_file(f"/tmp/{ prefix }.zip", bucket_name, key)
+        print(f"S3 uploaded response for {prefix}: {response}")
+        print(f"S3 {prefix} uploaded to s3://{bucket_name}/{key}")
     with open("/tmp/FuzzLean/ResponseBuckets/runSummary.json", "r") as f:
         results = json.load(f)        
     return results
+
