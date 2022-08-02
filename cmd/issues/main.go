d6fa3458f7102e2c8179fc2ab675cf0347b5d4d4
thefishua <josh.fishua@gmail.com>
2022-08-02 11:36:13 +1000

    Fixed main.go with Warn Logs and fixed naming of terraform resources and policies

--- cmd/issues/main.go
+++ cmd/issues/main.go
@@ -13,12 +13,12 @@ func main() {
 
 // Testing SQS message is sent to Lambda with the correct message contents
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
-	logger.Info("Function invoked!")
+	logger.Warn("Function invoked!")
 	if len(sqsEvent.Records) == 0 {
-		logger.Info("No SQS events")
+		logger.Warn("No SQS events")
 	}
 	for _, message := range sqsEvent.Records {
-		logger.Infof("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+		logger.Warnf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
 	}
 	return nil
 }
