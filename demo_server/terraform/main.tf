d37e6c1a44f92301e356f58ccb7d61b23f0bfb8a
Nicholas <n.j.stevens@student.unsw.edu.au>
2022-08-03 15:39:16 +1000

    deploy-app

--- demo_server/terraform/main.tf
+++ demo_server/terraform/main.tf
@@ -121,6 +121,7 @@ resource "aws_elastic_beanstalk_environment" "demo_server" {
   name                = "${var.deployment_id}-demo-server-environment"
   application         = aws_elastic_beanstalk_application.demo_server.name
   solution_stack_name = "64bit Amazon Linux 2 v3.4.17 running Docker"
+  version_label       = aws_elastic_beanstalk_application_version.demo_server.name
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
