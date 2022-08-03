aed5441744580b3281b993e699ee98f894edf16a
stevensnicholas <n.j.stevens@student.unsw.edu.au>
2022-08-03 13:28:39 +1000

    Fix restler (#176)
    * Add logger, fix missing f
    
    * .

--- restler/app/app.py
+++ restler/app/app.py
@@ -4,13 +4,17 @@ import json
 import boto3
 import os
 import shutil
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
     # Restler consistently tries to save files to working directory
     # So to avoid read only errors in lambda change wdir to /tmp
     os.chdir("/tmp")
-    print("logging started")
+    logger.info("logging started")
     restler_compile_cmd = "dotnet /RESTler/restler/Restler.dll --workingDirPath /tmp  compile --api_spec /tmp/swagger.json "
     restler_fuzz_cmd = r"""
     dotnet /RESTler/restler/Restler.dll \
@@ -24,16 +28,16 @@ def handler(event, context):
     """
     with open("/tmp/swagger.json", "w") as f:
         json.dump(event["swagger_file"], f)
-    print("swagger file saved")
+    logger.info("swagger file saved")
     run(restler_compile_cmd, shell=True)
     with open("/tmp/Compile/engine_settings.json", "r") as f:
         engine_settings = json.load(f)
         engine_settings["garbage_collection_interval"] = 0
     with open("/tmp/Compile/engine_settings.json", "w") as f:
-        json.dump("/tmp/Compile/engine_settings.json")
-    print("swagger file complied")
+        json.dump("/tmp/Compile/engine_settings.json", f)
+    logger.info("swagger file complied")
     run(restler_fuzz_cmd, shell=True)
-    print("fuzzing-lean complete")
+    logger.info("fuzzing-lean complete")
     s3 = boto3.client("s3")
     bucket_name = os.environ["results_upload_s3_bucket"]
     random_prefix = uuid.uuid4()
@@ -45,8 +49,8 @@ def handler(event, context):
         key = f"{random_prefix}/{prefix}.zip"
         shutil.make_archive(f"/tmp/{prefix}", "zip", f"/tmp/{folder}")
         response = s3.upload_file(f"/tmp/{ prefix }.zip", bucket_name, key)
-        print(f"S3 uploaded response for {prefix}: {response}")
-        print(f"S3 {prefix} uploaded to s3://{bucket_name}/{key}")
+        logger.info(f"S3 uploaded response for {prefix}: {response}")
+        logger.info(f"S3 {prefix} uploaded to s3://{bucket_name}/{key}")
     with open("/tmp/FuzzLean/ResponseBuckets/runSummary.json", "r") as f:
         results = json.load(f)
     return results
