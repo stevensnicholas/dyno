f2b377a078c595352cd43bc1ab755a1a935873e2
stevensnicholas <n.j.stevens@student.unsw.edu.au>
2022-08-04 20:23:10 +1000

    Allow s3 url as an input into restler  (#191)
    * Allow s3 url as in
    
    * .

--- restler/app/app.py
+++ restler/app/app.py
@@ -5,6 +5,7 @@ import boto3
 import os
 import shutil
 import logging
+from urllib.parse import urlparse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,7 +16,8 @@ def handler(event, context):
     # So to avoid read only errors in lambda change wdir to /tmp
     os.chdir("/tmp")
     logger.info("logging started")
-    restler_compile_cmd = "dotnet /RESTler/restler/Restler.dll --workingDirPath /tmp  compile --api_spec /tmp/swagger.json "
+    local_api_spec_path = "/tmp/swagger.json"
+    restler_compile_cmd = f"dotnet /RESTler/restler/Restler.dll --workingDirPath /tmp  compile --api_spec {local_api_spec_path} "
     restler_fuzz_cmd = r"""
     dotnet /RESTler/restler/Restler.dll \
         --workingDirPath /tmp \
@@ -26,19 +28,29 @@ def handler(event, context):
         --settings /tmp/Compile/engine_settings.json \
         --no_ssl
     """
-    with open("/tmp/swagger.json", "w") as f:
-        json.dump(event["swagger_file"], f)
-    logger.info("swagger file saved")
+
+    s3 = boto3.client("s3")
+    if "s3_location" in event:
+        swagger_url = urlparse(event["s3_location"])
+        s3.download_file(swagger_url.netloc, swagger_url.path, local_api_spec_path)
+    elif "swagger_json" in event:
+        with open(local_api_spec_path, "w") as f:
+            json.dump(event["swagger_json"], f)
+    else:
+        raise KeyError(
+            "No swagger file provided: Input needs to have either s3_location or swagger_json key"
+        )
+    logger.info(f"swagger file saved at {local_api_spec_path}")
     run(restler_compile_cmd, shell=True)
+    logger.info("swagger file complied")
+    # force no Garbage collection, not supported in lambda
     with open("/tmp/Compile/engine_settings.json", "r") as f:
         engine_settings = json.load(f)
         engine_settings["garbage_collection_interval"] = 0
     with open("/tmp/Compile/engine_settings.json", "w") as f:
         json.dump(engine_settings, f)
-    logger.info("swagger file complied")
     run(restler_fuzz_cmd, shell=True)
     logger.info("fuzzing-lean complete")
-    s3 = boto3.client("s3")
     bucket_name = os.environ["results_upload_s3_bucket"]
     random_prefix = uuid.uuid4()
     for folder, prefix in [
