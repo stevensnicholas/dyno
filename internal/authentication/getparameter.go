b42ebd2a1a273597d74e638e4fcaff1b4c60b0a0
MrC1rcle <liyf19970910@163.com>
2022-08-10 14:48:21 +1000

    delete unused fmt library

--- internal/authentication/getparameter.go
+++ internal/authentication/getparameter.go
@@ -3,7 +3,6 @@ package authentication
 import (
 	"context"
 	"dyno/internal/logger"
-	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
