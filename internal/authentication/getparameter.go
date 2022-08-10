5155eef07f7d7bb055663e246274713788e07924
MrC1rcle <liyf19970910@163.com>
2022-08-10 14:39:02 +1000

    add error check in getSSMParameterValue

--- internal/authentication/getparameter.go
+++ internal/authentication/getparameter.go
@@ -3,16 +3,18 @@ package authentication
 import (
 	"context"
 	"dyno/internal/logger"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
-func getSSMParameterValue(parameterName string) string {
+func getSSMParameterValue(parameterName string) (string, error) {
 
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	cfg, err := config.LoadDefaultConfig(context.TODO())//, config.WithRegion("ap-southeast-2")
 	if err != nil {
 		logger.Error(err.Error())
+		return "", err
 	}
 
 	svc := ssm.NewFromConfig(cfg)
@@ -22,8 +24,9 @@ func getSSMParameterValue(parameterName string) string {
 	})
 	if err != nil {
 		logger.Error(err.Error())
+		return "", err
 	}
 
-	return *resp.Parameter.Value
+	return *resp.Parameter.Value, nil
 
 }
